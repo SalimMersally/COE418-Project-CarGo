3bb1b69e0b260b11eaeaa4c441c44dd579c85091
Ahmad Al Sabsabi <ahmadalsabsabii@gmail.com>
2023-04-19 17:36:43 +0300

    Address Controller and Repository

---
+++ service/src/main/java/com/lau/cargo/controller/AddressController.java
@@ -0,0 +1,63 @@
+package com.lau.cargo.controller;
+
+import com.lau.cargo.entity.Address;
+import com.lau.cargo.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+    @Autowired
+    private AddressRepository addressRepository;
+    @GetMapping()
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+    @GetMapping(value = "/{addressId}")
+    public ResponseEntity<?> getAddressById(@PathVariable(value = "addressId") Long addressId) {
+        Optional<Address> address;
+        if(addressRepository.existsById(addressId)) {
+            address = addressRepository.findById(addressId);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        return  ResponseEntity.ok().body(address);
+    }
+    @PostMapping()
+    public Address createAddress(@Validated @RequestBody Address address) {
+        return addressRepository.save(address);
+    }
+    @PutMapping("/{addressId}")  //update
+    public ResponseEntity<?> updateAddress(@PathVariable(value = "addressId") Long addressId,
+                                           @Validated @RequestBody Address addressDetails) {
+        final Address updatedAddress;
+        if(addressRepository.existsById(addressId)) {
+            Address address = addressRepository.findById(addressId).orElseThrow(()-> new RuntimeException("not found"));
+            address.setAddress(addressDetails.getAddress());
+            address.setCity(addressDetails.getCity());
+            address.setState(addressDetails.getState());
+            address.setStreet(addressDetails.getStreet());
+
+            updatedAddress = addressRepository.save(address);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedAddress);
+    }
+    @DeleteMapping("/{addressId}")
+    public ResponseEntity<?> deleteAddress(@PathVariable(value = "addressId") Long addressId) throws Exception {
+        Address address;
+        if(addressRepository.existsById(addressId)) {
+            address = addressRepository.findById(addressId).orElseThrow(()-> new RuntimeException("not found"));
+            addressRepository.delete(address);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(address);
+    }
+}
